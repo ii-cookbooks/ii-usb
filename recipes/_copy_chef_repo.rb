0909dcce61b2e94229b9e1c9a599334c04fcb23e
Hippie Hacker <chris@hippiehacker.org>
2013-03-12 10:31:20 -0700

    update caching

--- recipes/_copy_chef_repo.rb
+++ recipes/_copy_chef_repo.rb
@@ -4,6 +4,8 @@ target_repo_dir = "#{node['ii-usb']['target-mountpoint']}/chef-repo"
 directory target_repo_dir
 
 # This is not idempotent, and is an ugly hack... fixme later
+# --size-only  isn't good for code... only binary ingredients/artifacts
+# that don't change
 execute 'rsync chef-repo onto usb' do
   command "rsync --modify-window=1 -rLptDv --exclude /.cache --exclude /.chef/cache --exclude /vendor #{node['ii-usb']['src-chef-repo']}/ #{target_repo_dir}/"
   # not_if 
